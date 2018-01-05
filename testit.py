12b8984aa8929fbc2ae3e74553727141110cf8b5
Jonek <pawel.jonski9@gmail.com>
2018-01-05 01:14:32 +0100

    zbieranie wartosci

--- testit.py
+++ testit.py
@@ -6,20 +6,31 @@ import sys
 i=0
 j=0
 
-REPEAT = 1
+REPEAT = 20
+missed = 0
 while i < REPEAT:
     process = Popen(["python3", "./graph_generator.py", "10", "0.5"], stdout=PIPE)
     (output, err) = process.communicate()
     exit_code = process.wait()
 
-    process = Popen(["python3", "./colann.py"], stdin=PIPE, stdout=PIPE)
+    process = Popen(["python3", "./colann.py", "0.95", "10", "100"], stdin=PIPE, stdout=PIPE)
     (output2, err) = process.communicate(input=output)
     exit_code = process.wait()
-    i += 1
-
+    if (exit_code != 0):
+        missed += 1
+        continue
+    else:
+        i += 1
     p = re.compile('LPPL: (.*)\n')
     changes = p.match(output2)
     changes = changes.group(1)
     changes = int(changes)
-    print("Changes: " + str(changes))
-    #print(str(i) + ": " + str(exit_code))
+
+    ppa = re.compile('Cost of solution: (.*)\n')
+    cost = ppa.search(output2)
+    cost = cost.group(1)
+    cost = int(cost)
+
+    print("Changes: " + str(changes) + " cost: " + str(cost))
+    #print(output2)
+print("Missed: " + str(missed))
