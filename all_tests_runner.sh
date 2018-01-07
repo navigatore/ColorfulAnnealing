670e56dcfe5de536fa319f8d04678c718352bd60
Wojciech Chlapek <wojciech.chlapek@tlen.pl>
2018-01-07 15:30:32 +0100

    Minor modifications in tests runner

---
+++ all_tests_runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+./testit.py 10 > test_inittemp_10.txt & 
+./testit.py 9 > test_inittemp_9.txt &
+./testit.py 8 > test_inittemp_8.txt &
+./testit.py 7 > test_inittemp_7.txt &
+./testit.py 6 > test_inittemp_6.txt &
+./testit.py 5 > test_inittemp_5.txt &
+./testit.py 4 > test_inittemp_4.txt &
+./testit.py 3 > test_inittemp_3.txt &
+./testit.py 2 > test_inittemp_2.txt &
+./testit.py 1 > test_inittemp_1.txt &
--- testit.py
+++ testit.py
@@ -1,47 +1,61 @@
-#!/usr/bin/python
+#!/usr/bin/python2
+from __future__ import print_function
 from subprocess import Popen, PIPE
+
 import re
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('init_temp', type=float, help='initial temperature')
+args = parser.parse_args()
 
 i=0
 
-REPEAT = 20
+REPEAT = 2
 missed = 0
 
-graph_sizes = [10, 15]
-probabs = [0.5, 0.6]
-INIT_TEMP = 10
-OUTER_LOOP = 10
-INNER_LOOP = 100
+graph_sizes = [1000, 5000]
+probabs = [0.1, 0.005]
+OUTER_LOOP = 100
+INNER_LOOP = 1000
 
 for graph_size in graph_sizes:
     for probab in probabs:
         i = 0
         missed = 0
-        print("GRAPH SIZE: " + str(graph_size) + " probability: " + str(probab))
-        while i < REPEAT:
+        
+        jumps_avg = 0
+        cost_avg = 0
+        for i in range(REPEAT):
+            
             process = Popen(["python3", "./graph_generator.py", str(graph_size), str(probab)], stdout=PIPE)
             (output, err) = process.communicate()
             exit_code = process.wait()
 
-            process = Popen(["python3", "./colann.py", str(INIT_TEMP), str(OUTER_LOOP), str(INNER_LOOP)], stdin=PIPE, stdout=PIPE)
+            process = Popen(["python3", "./colann.py", str(args.init_temp), str(OUTER_LOOP), str(INNER_LOOP)], stdin=PIPE, stdout=PIPE)
             (output2, err) = process.communicate(input=output)
             exit_code = process.wait()
             if (exit_code != 0):
                 missed += 1
+                print("Illegal solution, n =", graph_size, ", p =", probab, ", init temp =", args.init_temp, file=sys.stderr)
                 continue
-            else:
-                i += 1
 
             p = re.compile('LPPL: (.*)\n')
-            changes = p.search(output2)
-            changes = changes.group(1)
-            changes = float(changes)
+            jumps = p.search(output2)
+            jumps = jumps.group(1)
+            jumps = float(jumps)
+            jumps_avg += jumps
 
             ppa = re.compile('Cost of solution: (.*)\n')
             cost = ppa.search(output2)
             cost = cost.group(1)
             cost = int(cost)
+            cost_avg += cost
 
-            print("Jumps " + str(changes) + "% cost: " + str(cost))
-        print("Missed: " + str(missed))
+        jumps_avg /= REPEAT
+        cost_avg /= REPEAT
+
+        
+        print("n =", graph_size, ", p =", probab, ", init temp =", args.init_temp, ", jumps =", jumps, "%, cost =", cost, ", missed = ", missed)
+
